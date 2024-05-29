513921e796debda50a2d9ed8091b223508f8a9af
Andressa Rabêlo <dre.emy89@gmail.com>
2024-05-28 21:50:02 -0300

    cc

--- SangueAmigo/src/main/java/com/mycompany/sangueamigo/AgendarExames.java
+++ SangueAmigo/src/main/java/com/mycompany/sangueamigo/AgendarExames.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.sangueamigo;
+age com.mycompany.sangueamigo;
 
-/**
- *
- * @author DreEm
- */
+
 public class AgendarExames {
-    
+      private int codigodoacao_;
+      private String localdoacoes;
+      
+      private String datadeNascimento;
+      private String endereco;
+      private String tipoSanguineo;
+      private String senha_usuario;
+
+      //Conferir
+    public Usuario(int codigo, String nome, String senha) {
+        this.codigo = codigo;
+        this.nome_usuario = nome;
+        this.endereco = endereco;
+        this.datadeNascimento = datadeNascimento;
+        this.tipoSanguineo = tipoSanguineo;
+        this.senha_usuario = senha;
+    }
+    public Usuario (){
+        this.codigo = 0;
+        this.nome_usuario = "";
+        this.endereco = "";
+        this.datadeNascimento = "";
+        this.tipoSanguineo = "";
+        this.senha_usuario = "";
+    }
+    public Usuario (String login, String senha){
+        this.codigo = 0;
+        this.nome_usuario = login;
+        this.senha_usuario = senha;
+    }
 }
