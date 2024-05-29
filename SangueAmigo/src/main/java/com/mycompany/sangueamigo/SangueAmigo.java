2b9500d0502ee8fae4d6d753acfe040cb85cb584
Andressa Rabêlo <dre.emy89@gmail.com>
2024-05-28 21:02:06 -0300

    cc

---
+++ SangueAmigo/src/main/java/com/mycompany/sangueamigo/SangueAmigo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sangueamigo;
+
+/**
+ *
+ * @author DreEm
+ */
+public class SangueAmigo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
