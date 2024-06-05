f93af1a5ba62d9b21e59ef9449820c345b9e2d99
Andressa Rabêlo <dre.emy89@gmail.com>
2024-06-04 21:52:59 -0300

    cc

---
+++ SangueAmigo2/SangueAmigo2/src/main/java/com/mycompany/sangueamigo2/exameDAO.java
@@ -0,0 +1,49 @@
+
+package com.mycompany.sangueamigo2;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class exameDAO {
+    public void insert(Exame exame) {
+       
+       //Com auto increment
+      /*String sql =   "INSERT INTO usuario (nome,senha) VALUES ('"
+       +usuario.getNome() + "','"
+       + usuario.getSenha() + "')";
+      */
+      
+       String sql =   "INSERT INTO agendardoacao (local_exame, data_exame, hora_exame, opcaoExame, codigoExame) VALUES ('"
+       + exame.getLocal_exame() + "','"
+       + exame.getData_exame() + "','"        
+       + exame.getHora_exame() + "','"
+       + exame.getOpcaoExame() + "','"      
+       + exame.getCodigoExame() + "')" ;
+      
+      
+     
+       System.out.println(sql);
+        //Usado para tentar mapear o erro do código. Vai tentar executar a conexão com o banco de dados, caso não ocorra, ele vai te informar o erro (podemos detalhar futuramente as possibilidades de erros)
+        //Passo muito importande, devido a conexão Cliente - Servidor.
+       try {
+        Connection c = ConexaoDB.obtemConexao();
+        PreparedStatement ps = c.prepareStatement(sql);
+        ps.execute();
+        JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso");	
+        ps.close();
+        c.close();               
+
+        //classe usada para tratamentos de erros sql
+        } catch (SQLException e) {
+        JOptionPane.showMessageDialog(null, "Erros na Transação");
+        //método para imprimir o rastreamento da pilha de chamadas (ajudaa identificar a origem e o contexto do erro
+        e.printStackTrace();
+       }
+    }
+}
+
+
+
